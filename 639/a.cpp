1b4c94bf387d645a49cd61afd81b2c5b243e10b7
Abunyawa <abusaid.manap@gmail.com>
2020-05-08 02:11:59 +0600

    639 added

---
+++ 639/a.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define F first
+#define S second
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define abu ios_base::sync_with_stdio(0)
+#define said cin.tie(0)
+using namespace std;
+
+void yes(){
+    cout<<"YES"<<'\n';
+}
+
+void no(){
+    cout<<"NO"<<'\n';
+}
+
+void solve(){
+    ll n,m;
+    cin>>n>>m;
+    if(n==1 || m==1){
+        yes();
+    }else if(n==2 && m==2){
+        yes();
+    }else{
+        no();
+    }
+}
+
+int main(){
+    abu;
+    said;
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
