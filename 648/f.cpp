206b263983bfe3087e490a3bf79622c9fd7a183c
Abunyawa <abusaid.manap@gmail.com>
2020-06-08 15:31:56 +0600

    edu added and some problems solved

---
+++ 648/f.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define abu ios_base::sync_with_stdio(0)
+#define said cin.tie(0)
+using namespace std;
+ll const MOD = 998244353;
+
+void yes(){
+    cout<<"YES"<<'\n';
+}
+
+void no(){
+    cout<<"NO"<<'\n';
+}
+
+ll fact(int n){
+    ll ans = 1;
+    for(int i=2;i<=n;i++){
+        ans = (ans*i)%MOD;
+    }
+
+    return ans;
+}
+
+ll powM(int n, int x){
+    if(x==0){
+        return 1;
+    }
+    if(x==1){
+        return n%MOD;
+    }
+
+    if(x%2==0){
+        ll a = powM(n,x/2);
+        return (a*a)%MOD;
+    }else{
+        return (n*powM(n,x-1))%MOD;
+    }
+}
+
+ll C(int n, int k){
+    return (fact(n) * powM((fact(k)*fact(n-k))%MOD,MOD-2))%MOD;
+}
+
+ll A(int n, int k){
+    return (fact(n) * powM(fact(n-k)%MOD,MOD-2))%MOD;
+}
+
+void add_self(int &a, int b){
+    a += b;
+    if(a>=MOD){
+        a-=MOD;
+    }
+}
+
+void sub_self(int &a, int b){
+    a -= b;
+    if(a<0){
+        a+=MOD;
+    }
+}
+
+int bin[510][65];
+bool use[510];
+int mb[510];
+
+void solve(){
+    int n;
+    cin>>n;
+    vl a(n);
+    vl b(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+    if(n%2==1){
+        if(a[n/2]!=b[n/2]){
+            no();
+            return;
+        }
+    }
+    vector<pii> checkA;
+    vector<pii> checkB;
+    for(int i = 0;i<n/2;i++){
+        checkA.pb({max(a[i],a[n-1-i]),min(a[i],a[n-1-i])});
+        checkB.pb({max(b[i],b[n-1-i]),min(b[i],b[n-1-i])});
+    }
+    sort(all(checkA));
+    sort(all(checkB));
+    for(int i=0;i<checkA.size();i++){
+        if(checkA[i]!=checkB[i]){
+            no();
+            return;
+        }
+    }
+
+    yes();
+}
+
+int main(){
+    abu;
+    said;
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
